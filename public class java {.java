82b932ff57154d756476d1e6737fcd1ae220618c
melda2300 <146105556+melda2300@users.noreply.github.com>
2023-09-30 00:41:06 +0300

    Add files via upload

---
+++ public class java {.java
@@ -0,0 +1,29 @@
+public class java {
+
+
+    public class Main {
+        public static void main(String[] args) {
+            int i =9;
+            int j =5;
+            int y= 1009;
+            int x =1234;
+            System.out.println("y =" + y);
+            //Toplama işlemi
+            System.out.println("Toplama...");
+            System.out.println(" i + j = " + (i + j));
+            System.out.println(" x + y = " + (x + y));
+            //Çıkarma işlemi
+            System.out.println("Çıkarma...");
+            System.out.println(" i - j = " + (i - j));
+            System.out.println(" x - y = " + (x - y));
+
+            //Çarpma işlemi
+            System.out.println("Çarpma...");
+            System.out.println(" i * j = " + (i * j));
+            System.out.println(" x * y = " + (x * y));
+            //Bölme İşlemi
+            System.out.println("Bölme...");
+            System.out.println(" i / j = " + (i / j));
+            System.out.println(" x / y = " + (x / y));
+
+        }
